fd55da83928e3c0971928f33331308763ab99e84
erkizie <erkizie@gmail.com>
2025-01-10 20:05:13 +0100

    Polish models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,27 +1,10 @@
+# frozen_string_literal: true
+
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
 
   validates :title, presence: true
-
-  def original_ingredients
-    ingredients_details
-  end
-
-  def parsed_ingredient_names
-    ingredients_details.map do |detail|
-      extract_ingredient_name(detail)
-    end
-  end
-
-  private
-
-  def extract_ingredient_name(full_ingredient)
-    clean = full_ingredient.gsub(%r{^\d+([/\d\s]*)?}, '')
-    clean = clean.gsub(/\b(cup|cups|teaspoon|tablespoon|ounce|oz|can|bottle|quart|pound|grams|kg|ml|liter)\b/i, '')
-    clean = clean.gsub(/\(.*?\)/, '')
-    clean.strip.downcase
-  end
 end
 
 # == Schema Information
